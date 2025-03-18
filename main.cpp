efc381455532ab392d155d76fad26013d789f882
Yuhan_233 <1536943817@qq.com>
2025-03-18 13:28:33 +0800

    test again

--- main.cpp
+++ main.cpp
@@ -1,9 +1,7 @@
 #include <iostream>
 using namespace  std;
 int main(){
-    for(int i = 0; i < 10; i++){
-        printf("Hello World!\n");
-    }
+    cout << "Hello World";
     system("pause");
     return 0;
 }
